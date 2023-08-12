7a1873990e654e3add6ccc7ea9b9841d802ffc47
ryan-gillies <ryan.gillies@affirm.com>
2023-08-12 15:27:11 -0400

    Configure venmo setup for automated payouts

---
+++ side_pools/venmo.py
@@ -0,0 +1,17 @@
+from venmo_api import Client
+from credentials import getToken
+
+# Get 1Password uuid for Venmo API credential. Get access token stored in 1Password and connect to Venmo client
+venmo_uuid="fem2trbtqnwjpyn2cmqqhl5gju"
+venmo_access_token = getToken(venmo_uuid)
+venmo = Client(access_token=venmo_access_token)
+
+# Get user ID for given username
+recipient = venmo.user.get_user_by_username('Samantha-Pezzimenti').id
+
+# Send money from default payment source
+venmo.payment.send_money(amount=1.00,note="test",target_user_id=recipient)
+
+# payment_methods = venmo.payment.get_payment_methods()
+# for p in payment_methods:
+#     print(p)
