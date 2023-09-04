dc9c497c49be348a89e0622b25de20e67e21362e
ryan-gillies <ryan.gillies@affirm.com>
2023-09-04 15:58:27 -0400

    Update main.py

--- side_pools/main.py
+++ side_pools/main.py
@@ -1,13 +1,25 @@
-from pools import createPools
-import sleeper
+import pools
+import pandas
+import venmo
+import sqlalchemy
+
+engine = sqlalchemy.create_engine('postgresql://retool:om9EQK8HsJfh@ep-icy-salad-54868721.us-west-2.retooldb.com/retool?sslmode=require')                
 
 def main():
-    pool_list = createPools()
-    for pool in pool_list:
-        if pool['week'] == sleeper.week : 
-            ## Calculate winner
-            print(pool)
-        else:
-            pass
+    pool_list = pools.createPools()
+    weekly_winners = pools.calculatePools(pool_list)
+    payout_list = pools.payouts(pool_list,weekly_winners)
+    
+    df = pandas.DataFrame(payout_list)
+    print(df)
+    confirmation = input("Please confirm that the payouts are accurate (y/n)")
+    
+    if confirmation == 'y':
+        df.to_sql(name='payouts', con=engine, if_exists='append')
+        for p in payout_list:
+            venmo.payout(p['username'],p['payout'],p['pool']+':week'+p['week'])
+        print('Hooray')
+    else:
+        exit()
 
 main()
